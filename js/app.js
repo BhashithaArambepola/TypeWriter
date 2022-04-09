c2f74263b0fffc8a204491580b9fd0aeae765d88
bhashitha Arambepola <barambepola@gmail.com>
2022-04-09 13:48:39 +0530

    add JS for type writer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
-console.log("working")
+var texts = [
+    'Web Developer',
+    'Ui/Ux designer',
+    "Fullstack Developer"
+];
+var startingTime = null;
+var i = 0;
+var j = 0;
+var k = 0;
+var elmStage = document.getElementById('stage');
+var reverse = false;
+
+
+function animateText(stamp) {
+
+    if (!startingTime) {
+        startingTime = stamp;
+    }
+
+    var interval = stamp - startingTime;
+
+    if (interval >= 70 && reverse) {
+        var text = texts[j];
+        elmStage.innerText = text.substring(0, k--);
+
+        if (k == 0){
+            j++;
+            reverse = false;
+
+            if (j >= texts.length) j = 0;
+        }
+    }
+
+    if (interval >= 150 && !reverse) {
+        // var text = text[j];
+        startingTime = stamp;
+
+        var text = texts[j];
+        elmStage.innerText = text.substring(0, i++);
+        if (i > (text.length + 10)) {
+            reverse=true;
+            i = 0;
+            k = text.length;
+        }
+    }
+
+    requestAnimationFrame(animateText);
+
+}
+requestAnimationFrame(animateText);
